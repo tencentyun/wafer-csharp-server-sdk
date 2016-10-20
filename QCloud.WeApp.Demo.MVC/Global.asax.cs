b671042f408d8aab966607ce5531df022d7e7d82
techird <techird@qq.com>
2016-10-20 16:26:46 +0800

    去除无用引用

--- QCloud.WeApp.Demo.MVC/Global.asax.cs
+++ QCloud.WeApp.Demo.MVC/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace QCloud.WeApp.Demo.MVC
@@ -15,7 +14,6 @@ namespace QCloud.WeApp.Demo.MVC
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
             QCloudConfig.Setup();
         }
     }
